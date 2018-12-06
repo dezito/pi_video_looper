c4585439260cc7a0674a626ead7a7dc0ad7ae3df
Tim <airyimbin@gmail.com>
2018-12-06 15:35:26 -0500

    feh

--- Adafruit_Video_Looper/omxplayer.py
+++ Adafruit_Video_Looper/omxplayer.py
@@ -31,7 +31,7 @@ class OMXPlayer(object):
     def play_image(self, image):
         self.stop(3)
         args = ['feh']
-	args.extend('-Z -z -F --hide-pointer --cycle-once')
+	args.extend(['-Z -z -F --hide-pointer --cycle-once'])
         args.extend(['-D', self._imageDelay])
         args.append(image)
         self._process = subprocess.Popen(args, stdout=open(os.devnull, 'wb'), close_fds=True)
