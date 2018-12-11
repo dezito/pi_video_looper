a195e9ad1ed6822b88586830f3791744762db416
Tim <airyimbin@gmail.com>
2018-12-11 14:00:35 -0500

    sleep

--- Adafruit_Video_Looper/directory.py
+++ Adafruit_Video_Looper/directory.py
@@ -11,7 +11,7 @@ class DirectoryReader(object):
         directory on disk.
         """
         self._load_config(config)
-        self._mtimes = {}
+        self._mtimes = None
 
     def _load_config(self, config):
         self._path = config.get('directory', 'path')
